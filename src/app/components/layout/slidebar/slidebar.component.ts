a5775b562df318459755c09a0882aa36a89f927a
olivo1990 <camilo.olivo1990@gmail.com.com>
2020-01-17 17:16:09 -0500

    Se realizan los cambios para mostrar el menu

--- src/app/components/layout/slidebar/slidebar.component.ts
+++ src/app/components/layout/slidebar/slidebar.component.ts
@@ -11,6 +11,7 @@ import { CompileShallowModuleMetadata } from '@angular/compiler';
 export class SlidebarComponent implements OnInit {
 
   menu: Menu[] = [];
+  menuArreglo = new Array();
 
   constructor(private usuarioService: UsuarioService) { }
 
@@ -18,6 +19,75 @@ export class SlidebarComponent implements OnInit {
     this.menu = this.usuarioService.menu;
 
     console.log(this.menu);
+  }
+
+
+  crearMenu(idPadreA:number):void{
+    if(Object.keys(this.menu).length !== 0){
+
+        for (let i in this.menu) {
+          let idMenu:number = this.menu[i]["id"];
+          let idPadreB:number = this.menu[i]["idPadre"];
+          if (idPadreA === idPadreB) {
+            if (idPadreB === 0) {
+              this.menuArreglo.push({'id':this.menu[i]["id"],'label':this.menu[i]["nombre"],'link':this.menu[i]["url"],'icon':this.menu[i]["icono"]});
+
+              this.crearMenu(idMenu);
+
+            }else{
+              if(this.menuTieneHijos(idMenu) > 0){
+                for (let p in this.menuArreglo) {
+
+                  if(this.menuArreglo[p]["id"] == idPadreA){
+                    let arregloHijo = {
+                      id: this.menu[i]["id"],
+                      label: this.menu[i]["nombre"],
+                      link: this.menu[i]["url"],
+                      icon: this.menu[i]["icono"]
+                    }
+
+                    if(this.menuArreglo[p]['items'] !== undefined){
+                      this.menuArreglo[p]['items'].push(arregloHijo);
+                    }else{
+                      this.menuArreglo[p]['items'] = [arregloHijo];
+                    }
+                  }
+                }
+                this.crearMenu(idMenu);
+              }else{
+                for (let p in this.menuArreglo) {
+                  if(this.menuArreglo[p]["id"] == idPadreA){
+                    let arregloHijo = {
+                      id: this.menu[i]["id"],
+                      label: this.menu[i]["nombre"],
+                      link: this.menu[i]["url"],
+                      icon: this.menu[i]["icono"]
+                    }
+
+                    if(this.menuArreglo[p]['items'] !== undefined){
+                      this.menuArreglo[p]['items'].push(arregloHijo);
+                    }else{
+                      this.menuArreglo[p]['items'] = [arregloHijo];
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+
+    }
+  }
+
+  menuTieneHijos (idPadreA:number):number {
+    let n = 0;
+    for (let i in this.menu) {
+      let idPadreB:number = this.menu[i]["idPadre"];
+      if (idPadreB === idPadreA) {
+        n += 1;
+      }
+    }
+    return n;
   }
 
 }
